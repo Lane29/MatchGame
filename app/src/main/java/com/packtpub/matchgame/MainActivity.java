641c0d476276e38d1a3c913a26bca71085274a6a
lane29 <lane29@me.com>
2018-09-14 22:15:30 +1200

    Initial commit

---
+++ app/src/main/java/com/packtpub/matchgame/MainActivity.java
@@ -0,0 +1,41 @@
+package com.packtpub.matchgame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static int flipTimeMsc = 200;
+
+    //Variables for the table of results
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //Start game - Level 1
+    public void onPlayClick(View view) {
+        Intent i;
+        i = new Intent(this, Level1Activity.class);
+        startActivity(i);
+    }
+
+    //Show the table of results
+    public void onResultsClick(View view) {
+        Intent i;
+        i = new Intent(this, ResultsActivity.class);
+        startActivity(i);
+    }
+
+    //Show the page with rules
+    public void onRulesClick(View view) {
+        Intent i;
+        i = new Intent(this, RulesActivity.class);
+        startActivity(i);
+    }
+}
